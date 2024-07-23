db4d0f4902e9e68eb42c19b3f4a900bee41caefa
kasrazarei39 <kasrazarei39@gmail.com>
2024-07-23 16:55:55 +0330

    Fix block insertion and orphan management test

--- src/cmg/cnsim/bitcoin/test/BlockchainTest.java
+++ src/cmg/cnsim/bitcoin/test/BlockchainTest.java
@@ -79,12 +79,20 @@ class BlockchainTest {
 
 
 	/**
-	 * Test method for {@linkplain cmg.cnsim.bitcoin.Blockchain#addToStructure(Block)}.
+	 * Tests the {@linkplain cmg.cnsim.bitcoin.Blockchain#addToStructure(Block)} method.
+	 * This method verifies the correct addition of blocks to the blockchain,
+	 * checking both the blockchain structure and orphan management.
+	 *
+	 * The following scenarios are tested:
+	 * 1. Adding a single block to the blockchain.
+	 * 2. Adding multiple blocks and verifying parent-child relationships.
+	 * 3. Handling orphan blocks correctly when their parents are later added to the blockchain.
+	 * 4. Ensuring the blockchain structure is maintained accurately after each addition.
+	 * 5. Checking the list of tips (blocks with no children) after various additions.
 	 */
 	@Test
-	final void testAddToStructure() {
-
-		//0
+	final void testBlockInsertionAndOrphanManagement() {
+		//1
 		Block block = new Block();
 		block.addTransaction(new Transaction(1,10,10,50));
 		block.addTransaction(new Transaction(2,11,20,25));
@@ -94,129 +102,129 @@ class BlockchainTest {
 		block.addTransaction(new Transaction(6,16,100,50));
 		blockchain.addToStructure(block);
 
-
+		// Checking the structure after adding the first block
 		String[] expected_1 = {"BlockID,ParentID,BlockHeight,Transactions",
-				"0,-1,1,{1,2,3,4,5,6}"};
+				"1,-1,1,{1,2,3,4,5,6}"};
 		assertArrayEquals(expected_1, blockchain.printStructure());
 
+		// Checking that there are no orphans
 		String[] oxpected_1 = {"BlockID,ParentID,Transactions"};
 		assertArrayEquals(oxpected_1, blockchain.printOrphans());
 
 
-		//1 --> 0
+		//2 --> 1
 		block = new Block();
 		block.addTransaction(new Transaction(7,19,10,55));
 		block.addTransaction(new Transaction(8,20,25,20));
 		block.addTransaction(new Transaction(9,21,105,10));
 		block.addTransaction(new Transaction(10,22,55,100));
-		Block keep_1 = block;
+		Block keep_2 = block;
 		blockchain.addToStructure(block);
 
-
 		String[] expected_2 = {"BlockID,ParentID,BlockHeight,Transactions",
-				"1,0,2,{7,8,9,10}",
-				"0,-1,1,{1,2,3,4,5,6}"};
+				"2,1,2,{7,8,9,10}",
+				"1,-1,1,{1,2,3,4,5,6}"};
 
 		assertArrayEquals(expected_2, blockchain.printStructure());
 		assertArrayEquals(oxpected_1, blockchain.printOrphans());
 
-		//2 --> 1
+
+		//3 --> 2
 		block = new Block();
 		block.addTransaction(new Transaction(11,23,10,505));
 		block.addTransaction(new Transaction(12,25,250,2));
 		block.addTransaction(new Transaction(13,30,505,10));
-		Block keepme_2 = block;
+		Block keep_3 = block;
 		blockchain.addToStructure(block);
 
-
 		String[] expected_3 = {"BlockID,ParentID,BlockHeight,Transactions",
-				"2,1,3,{11,12,13}",
-				"1,0,2,{7,8,9,10}",
-				"0,-1,1,{1,2,3,4,5,6}"};
+				"3,2,3,{11,12,13}",
+				"2,1,2,{7,8,9,10}",
+				"1,-1,1,{1,2,3,4,5,6}"};
+
 		assertArrayEquals(expected_3, blockchain.printStructure());
 		assertArrayEquals(oxpected_1, blockchain.printOrphans());
 
 
-		//3 --> 1
+		//4 --> 2
 		block = new Block();
 		block.addTransaction(new Transaction(14,35,10,505));
 		block.addTransaction(new Transaction(15,40,250,2));
-		block.setParent(keep_1);
-		Block keep2_3 = block;
+		block.setParent(keep_2);
+		Block keep_4_2 = block;
 		blockchain.addToStructure(block);
 
-		// Slide 1
 		String[] expected_4 = {"BlockID,ParentID,BlockHeight,Transactions",
-				"3,1,3,{14,15}",
-				"2,1,3,{11,12,13}",
-				"1,0,2,{7,8,9,10}",
-				"0,-1,1,{1,2,3,4,5,6}"};
+				"4,2,3,{14,15}",
+				"3,2,3,{11,12,13}",
+				"2,1,2,{7,8,9,10}",
+				"1,-1,1,{1,2,3,4,5,6}"};
+
 		assertArrayEquals(expected_4, blockchain.printStructure());
 		assertArrayEquals(oxpected_1, blockchain.printOrphans());
-		assertEquals(blockchain.printTips(","), "{2,3}");
+		assertEquals(blockchain.printTips(","), "{3,4}");
 
 
-
-		//4 --> 3
+		//5 --> 4
 		block = new Block();
 		block.addTransaction(new Transaction(16,41,10,505));
 		block.addTransaction(new Transaction(17,42,250,2));
 		assertNull(block.getParent());
 		blockchain.addToStructure(block);
 
-		//Slide 2
 		String[] expected_5 = {"BlockID,ParentID,BlockHeight,Transactions",
-				"4,3,4,{16,17}",
-				"3,1,3,{14,15}",
-				"2,1,3,{11,12,13}",
-				"1,0,2,{7,8,9,10}",
-				"0,-1,1,{1,2,3,4,5,6}"};
+				"5,4,4,{16,17}",
+				"4,2,3,{14,15}",
+				"3,2,3,{11,12,13}",
+				"2,1,2,{7,8,9,10}",
+				"1,-1,1,{1,2,3,4,5,6}"};
+
 		assertArrayEquals(expected_5, blockchain.printStructure());
 		assertArrayEquals(oxpected_1, blockchain.printOrphans());
-		assertEquals(blockchain.printTips(","), "{2,4}");
+		assertEquals(blockchain.printTips(","), "{3,5}");
 
 
-
-		//5 --> 4
+		//6 --> 5
 		block = new Block();
 		block.addTransaction(new Transaction(18,41,10,505));
 		block.addTransaction(new Transaction(19,42,250,2));
 		blockchain.addToStructure(block);
 
-		// Slide 3
 		String[] expected_6 = {"BlockID,ParentID,BlockHeight,Transactions",
-				"5,4,5,{18,19}",
-				"4,3,4,{16,17}",
-				"3,1,3,{14,15}",
-				"2,1,3,{11,12,13}",
-				"1,0,2,{7,8,9,10}",
-				"0,-1,1,{1,2,3,4,5,6}"};
+				"6,5,5,{18,19}",
+				"5,4,4,{16,17}",
+				"4,2,3,{14,15}",
+				"3,2,3,{11,12,13}",
+				"2,1,2,{7,8,9,10}",
+				"1,-1,1,{1,2,3,4,5,6}"};
+
 		assertArrayEquals(expected_6, blockchain.printStructure());
 		assertArrayEquals(oxpected_1, blockchain.printOrphans());
-		assertEquals(blockchain.printTips(","), "{2,5}");
+		assertEquals(blockchain.printTips(","), "{3,6}");
 
-		//6 --> 1
+
+		//7 --> 2
 		block = new Block();
 		block.addTransaction(new Transaction(20,25,10,505));
 		block.addTransaction(new Transaction(21,26,250,2));
-		block.setParent(keep_1);
+		block.setParent(keep_2);
 		blockchain.addToStructure(block);
 
 		String[] expected_7 = {"BlockID,ParentID,BlockHeight,Transactions",
-				"5,4,5,{18,19}",
-				"4,3,4,{16,17}",
-				"6,1,3,{20,21}",
-				"3,1,3,{14,15}",
-				"2,1,3,{11,12,13}",
-				"1,0,2,{7,8,9,10}",
-				"0,-1,1,{1,2,3,4,5,6}"};
+				"6,5,5,{18,19}",
+				"5,4,4,{16,17}",
+				"7,2,3,{20,21}",
+				"4,2,3,{14,15}",
+				"3,2,3,{11,12,13}",
+				"2,1,2,{7,8,9,10}",
+				"1,-1,1,{1,2,3,4,5,6}"};
 
 		assertArrayEquals(expected_7, blockchain.printStructure());
 		assertArrayEquals(oxpected_1, blockchain.printOrphans());
-		assertEquals("{2,5,6}",blockchain.printTips(","));
+		assertEquals("{3,6,7}",blockchain.printTips(","));
 
 
-		//7 --> 5
+		//8 --> 6
 		block = new Block();
 		block.addTransaction(new Transaction(22,41,10,505));
 		block.addTransaction(new Transaction(23,42,250,2));
@@ -224,224 +232,237 @@ class BlockchainTest {
 		blockchain.addToStructure(block);
 
 		String[] expected_8 = {"BlockID,ParentID,BlockHeight,Transactions",
-				"7,5,6,{22,23,24}",
-				"5,4,5,{18,19}",
-				"4,3,4,{16,17}",
-				"6,1,3,{20,21}",
-				"3,1,3,{14,15}",
-				"2,1,3,{11,12,13}",
-				"1,0,2,{7,8,9,10}",
-				"0,-1,1,{1,2,3,4,5,6}"};
+				"8,6,6,{22,23,24}",
+				"6,5,5,{18,19}",
+				"5,4,4,{16,17}",
+				"7,2,3,{20,21}",
+				"4,2,3,{14,15}",
+				"3,2,3,{11,12,13}",
+				"2,1,2,{7,8,9,10}",
+				"1,-1,1,{1,2,3,4,5,6}"};
 
 		assertArrayEquals(expected_8, blockchain.printStructure());
 		assertArrayEquals(oxpected_1, blockchain.printOrphans());
-		assertEquals("{6,2,7}",blockchain.printTips(","));
+		assertEquals("{7,3,8}",blockchain.printTips(","));
 
 
-		//8 --> 3
+		// Not added in the blockchain
+		//9 --> 4
 		block = new Block();
 		block.addTransaction(new Transaction(25,25,10,505));
 		block.addTransaction(new Transaction(26,26,250,2));
-		block.setParent(keep2_3);
-		Block keep3_8 = block;
+		block.setParent(keep_4_2);
+		Block keep_9_4 = block;
 
-		//9 --> 8
+
+		// Orphan Block
+		//10 --> 9
 		block = new Block();
 		block.addTransaction(new Transaction(27,25,10,505));
 		block.addTransaction(new Transaction(28,26,250,2));
-		block.setParent(keep3_8);
-		Block keep4_9 = block;
+		block.setParent(keep_9_4);
+		Block keep_10_9 = block;
 		blockchain.addToStructure(block);
 
-
 		String[] expected_9 = {"BlockID,ParentID,BlockHeight,Transactions",
-				"7,5,6,{22,23,24}",
-				"5,4,5,{18,19}",
-				"4,3,4,{16,17}",
-				"6,1,3,{20,21}",
-				"3,1,3,{14,15}",
-				"2,1,3,{11,12,13}",
-				"1,0,2,{7,8,9,10}",
-				"0,-1,1,{1,2,3,4,5,6}"};
+				"8,6,6,{22,23,24}",
+				"6,5,5,{18,19}",
+				"5,4,4,{16,17}",
+				"7,2,3,{20,21}",
+				"4,2,3,{14,15}",
+				"3,2,3,{11,12,13}",
+				"2,1,2,{7,8,9,10}",
+				"1,-1,1,{1,2,3,4,5,6}"};
 
 		assertArrayEquals(expected_9, blockchain.printStructure());
-		assertEquals("{6,2,7}",blockchain.printTips(","));
+		assertEquals("{7,3,8}",blockchain.printTips(","));
 
 		String[] oxpected_2 = {"BlockID,ParentID,Transactions",
-				"9,8,{27,28}"
+				"10,9,{27,28}"
 		};
 		assertArrayEquals(oxpected_2, blockchain.printOrphans());
-		assertEquals("{6,2,7}",blockchain.printTips(","));
 
-		//10 --> 9
+
+		//11 --> 10
 		block = new Block();
 		block.addTransaction(new Transaction(29,25,10,505));
 		block.addTransaction(new Transaction(30,26,250,2));
 		block.addTransaction(new Transaction(31,26,250,2));
-		block.setParent(keep4_9);
+		block.setParent(keep_10_9);
 		blockchain.addToStructure(block);
 
 		assertArrayEquals(expected_9, blockchain.printStructure());
+		assertEquals("{7,3,8}",blockchain.printTips(","));
 
 		String[] oxpected_3 = {"BlockID,ParentID,Transactions",
-				"9,8,{27,28}",
-				"10,9,{29,30,31}"
+				"10,9,{27,28}",
+				"11,10,{29,30,31}"
 		};
 		assertArrayEquals(oxpected_3, blockchain.printOrphans());
-		assertEquals("{6,2,7}",blockchain.printTips(","));
 
-		//Plug 8 now. Orphans must be placed back.
-		blockchain.addToStructure(keep3_8);
+		//Plug 9 now. Orphans must be placed back.
+		blockchain.addToStructure(keep_9_4);
 
 		String[] expected_10 = {"BlockID,ParentID,BlockHeight,Transactions",
-				"10,9,6,{29,30,31}",
-				"7,5,6,{22,23,24}",
-				"9,8,5,{27,28}",
-				"5,4,5,{18,19}",
-				"8,3,4,{25,26}",
-				"4,3,4,{16,17}",
-				"6,1,3,{20,21}",
-				"3,1,3,{14,15}",
-				"2,1,3,{11,12,13}",
-				"1,0,2,{7,8,9,10}",
-				"0,-1,1,{1,2,3,4,5,6}"};
+				"11,10,6,{29,30,31}",
+				"8,6,6,{22,23,24}",
+				"10,9,5,{27,28}",
+				"6,5,5,{18,19}",
+				"9,4,4,{25,26}",
+				"5,4,4,{16,17}",
+				"7,2,3,{20,21}",
+				"4,2,3,{14,15}",
+				"3,2,3,{11,12,13}",
+				"2,1,2,{7,8,9,10}",
+				"1,-1,1,{1,2,3,4,5,6}"};
 		assertArrayEquals(expected_10, blockchain.printStructure());
 		assertArrayEquals(oxpected_1, blockchain.printOrphans());
-		assertEquals("{6,2,7,10}",blockchain.printTips(","));
+		assertEquals("{7,3,8,11}",blockchain.printTips(","));
 
-		//11 --> 2
+
+		// Not added in the blockchain
+		//12 --> 3
 		block = new Block();
 		block.addTransaction(new Transaction(32,25,10,505));
 		block.addTransaction(new Transaction(33,26,250,2));
-		block.setParent(keepme_2);
-		Block keep_11 = block;
+		block.setParent(keep_3);
+		Block keep_12_3 = block;
 
-		//12 --> 11
+
+		// Orphan Block
+		//13 --> 12
 		block = new Block();
 		block.addTransaction(new Transaction(34,25,10,505));
 		block.addTransaction(new Transaction(35,26,250,2));
-		block.setParent(keep_11);
+		block.setParent(keep_12_3);
 		blockchain.addToStructure(block);
 
 		assertArrayEquals(expected_10, blockchain.printStructure());
+		assertEquals("{7,3,8,11}",blockchain.printTips(","));
+
 		String[] oxpected_4 = {"BlockID,ParentID,Transactions",
-				"12,11,{34,35}"
+				"13,12,{34,35}"
 		};
 		assertArrayEquals(oxpected_4, blockchain.printOrphans());
-		assertEquals("{6,2,7,10}",blockchain.printTips(","));
 
-
-		blockchain.addToStructure(keep_11);
+		//Plug 12 now. Orphans must be placed back.
+		blockchain.addToStructure(keep_12_3);
 
 		String[] expected_11 = {"BlockID,ParentID,BlockHeight,Transactions",
-				"10,9,6,{29,30,31}",
-				"7,5,6,{22,23,24}",
-				"12,11,5,{34,35}",
-				"9,8,5,{27,28}",
-				"5,4,5,{18,19}",
-				"11,2,4,{32,33}",
-				"8,3,4,{25,26}",
-				"4,3,4,{16,17}",
-				"6,1,3,{20,21}",
-				"3,1,3,{14,15}",
-				"2,1,3,{11,12,13}",
-				"1,0,2,{7,8,9,10}",
-				"0,-1,1,{1,2,3,4,5,6}"};
+				"11,10,6,{29,30,31}",
+				"8,6,6,{22,23,24}",
+				"13,12,5,{34,35}",
+				"10,9,5,{27,28}",
+				"6,5,5,{18,19}",
+				"12,3,4,{32,33}",
+				"9,4,4,{25,26}",
+				"5,4,4,{16,17}",
+				"7,2,3,{20,21}",
+				"4,2,3,{14,15}",
+				"3,2,3,{11,12,13}",
+				"2,1,2,{7,8,9,10}",
+				"1,-1,1,{1,2,3,4,5,6}"};
 		assertArrayEquals(expected_11, blockchain.printStructure());
 		assertArrayEquals(oxpected_1, blockchain.printOrphans());
-		assertEquals("{6,7,10,12}",blockchain.printTips(","));
+		assertEquals("{7,8,11,13}",blockchain.printTips(","));
 
-		//13 --> -1 (overlaps)
+
+		//14 (overlaps with block 4)
 		block = new Block();
 		block.addTransaction(new Transaction(14,25,10,505));
 		block.addTransaction(new Transaction(36,26,250,2));
-		Block keep_13 = block;
+		Block keep_14 = block;
 
-		//14 --> 13
+
+		//15 --> 14
 		block = new Block();
 		block.addTransaction(new Transaction(37,25,10,505));
 		block.addTransaction(new Transaction(38,26,250,2));
-		block.setParent(keep_13);
+		block.setParent(keep_14);
 		blockchain.addToStructure(block);
-
 
 		assertArrayEquals(expected_11, blockchain.printStructure());
 		String[] oxpected_5 = {"BlockID,ParentID,Transactions",
-				"14,13,{37,38}"
+				"15,14,{37,38}"
 		};
 		assertArrayEquals(oxpected_5, blockchain.printOrphans());
 
+		//It will be added to tip 13; because block 14 doesn't have overlaps with this tip
+		blockchain.addToStructure(keep_14);
 
-		//Does not matter.
-		blockchain.addToStructure(keep_13);
-
-		String[] expected_111 = {"BlockID,ParentID,BlockHeight,Transactions",
-				"14,13,7,{37,38}",
-				"13,12,6,{14,36}",
-				"10,9,6,{29,30,31}",
-				"7,5,6,{22,23,24}",
-				"12,11,5,{34,35}",
-				"9,8,5,{27,28}",
-				"5,4,5,{18,19}",
-				"11,2,4,{32,33}",
-				"8,3,4,{25,26}",
-				"4,3,4,{16,17}",
-				"6,1,3,{20,21}",
-				"3,1,3,{14,15}",
-				"2,1,3,{11,12,13}",
-				"1,0,2,{7,8,9,10}",
-				"0,-1,1,{1,2,3,4,5,6}"};
+		String[] expected_12 = {"BlockID,ParentID,BlockHeight,Transactions",
+				"15,14,7,{37,38}",
+				"14,13,6,{14,36}",
+				"11,10,6,{29,30,31}",
+				"8,6,6,{22,23,24}",
+				"13,12,5,{34,35}",
+				"10,9,5,{27,28}",
+				"6,5,5,{18,19}",
+				"12,3,4,{32,33}",
+				"9,4,4,{25,26}",
+				"5,4,4,{16,17}",
+				"7,2,3,{20,21}",
+				"4,2,3,{14,15}",
+				"3,2,3,{11,12,13}",
+				"2,1,2,{7,8,9,10}",
+				"1,-1,1,{1,2,3,4,5,6}"};
 		assertArrayEquals(oxpected_1, blockchain.printOrphans());
-		assertArrayEquals(expected_111, blockchain.printStructure());
-		assertEquals("{10,7,6,14}",blockchain.printTips(","));
+		assertArrayEquals(expected_12, blockchain.printStructure());
+		assertEquals("{11,8,7,15}",blockchain.printTips(","));
 
 
+		// 16 (overlaps with blocks: 11, 6, 3)
 		block = new Block();
 		block.addTransaction(new Transaction(29,25,10,505));
 		block.addTransaction(new Transaction(18,41,10,505));
 		block.addTransaction(new Transaction(12,25,250,2));
 		blockchain.addToStructure(block);
 
-		//Debug.p("This:"+blockchain.getNonOverlappingTip(block).getID());
-		//assertFalse(blockchain.hasChainOverlap(block, blockchain.getNonOverlappingTip(block)));
-		String[] expected = {"BlockID,ParentID,BlockHeight,Transactions",
-				"14,13,7,{37,38}",
-				"13,12,6,{14,36}",
-				"10,9,6,{29,30,31}",
-				"7,5,6,{22,23,24}",
-				"12,11,5,{34,35}",
-				"9,8,5,{27,28}",
-				"5,4,5,{18,19}",
-				"15,6,4,{29,18,12}",
-				"11,2,4,{32,33}",
-				"8,3,4,{25,26}",
-				"4,3,4,{16,17}",
-				"6,1,3,{20,21}",
-				"3,1,3,{14,15}",
-				"2,1,3,{11,12,13}",
-				"1,0,2,{7,8,9,10}",
-				"0,-1,1,{1,2,3,4,5,6}"};
+		String[] expected_13 = {"BlockID,ParentID,BlockHeight,Transactions",
+				"15,14,7,{37,38}",
+				"14,13,6,{14,36}",
+				"11,10,6,{29,30,31}",
+				"8,6,6,{22,23,24}",
+				"13,12,5,{34,35}",
+				"10,9,5,{27,28}",
+				"6,5,5,{18,19}",
+				"16,7,4,{29,18,12}",
+				"12,3,4,{32,33}",
+				"9,4,4,{25,26}",
+				"5,4,4,{16,17}",
+				"7,2,3,{20,21}",
+				"4,2,3,{14,15}",
+				"3,2,3,{11,12,13}",
+				"2,1,2,{7,8,9,10}",
+				"1,-1,1,{1,2,3,4,5,6}"};
 		assertArrayEquals(oxpected_1, blockchain.printOrphans());
-		assertArrayEquals(expected, blockchain.printStructure());
-		assertEquals("{14,10,7,15}",blockchain.printTips(","));
+		assertArrayEquals(expected_13, blockchain.printStructure());
+		assertEquals("{15,11,8,16}",blockchain.printTips(","));
 
 
+		//17 (overlaps with blocks: 13, 5, 11, 7)
+		// It won't be added to the blockchain. Because it has overlaps with all tips
 		block = new Block();
 		block.addTransaction(new Transaction(34,25,10,505));
 		block.addTransaction(new Transaction(16,41,10,505));
 		block.addTransaction(new Transaction(31,25,250,2));
 		block.addTransaction(new Transaction(20,25,250,2));
 		blockchain.addToStructure(block);
-		assertArrayEquals(oxpected_1, blockchain.printOrphans());
-		assertArrayEquals(expected, blockchain.printStructure());
-		assertEquals("{14,10,7,15}",blockchain.printTips(","));
 
+		assertArrayEquals(expected_13, blockchain.printStructure());
+		assertArrayEquals(oxpected_1, blockchain.printOrphans());
+		assertEquals("{15,11,8,16}",blockchain.printTips(","));
+
+
+		// 18 (Overlaps with genesis block)
+		// It won't be added to the blockchain. Because it has overlaps with genesis block
 		block = new Block();
 		block.addTransaction(new Transaction(1,25,10,505));
 		blockchain.addToStructure(block);
+
+		assertArrayEquals(expected_13, blockchain.printStructure());
 		assertArrayEquals(oxpected_1, blockchain.printOrphans());
-		assertArrayEquals(expected, blockchain.printStructure());
-		assertEquals("{14,10,7,15}",blockchain.printTips(","));
+		assertEquals("{15,11,8,16}",blockchain.printTips(","));
 	}
 
 	/**
