ab25cc0c0f452803e11cf56cea72b9a56788c46a
liaskos <liaskos@yorku.ca>
2024-11-13 11:21:32 -0500

    UPdating documentation

---
+++ docs/apidocs/javadoc.bat
@@ -0,0 +1,1 @@
+cmd.exe /X /D /C ""C:\Program Files\Java\jdk-23\bin\javadoc.exe" -J-Duser.language= -J-Duser.country= @options @packages"
---
+++ docs/index.md
@@ -0,0 +1,4 @@
+### Under construction
+## This is a page under construction.
+
+ONe wonders if it will work..
