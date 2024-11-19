432731090580accd7dc180067b70285508e02710
nina2dev <nina2dev@gmail.com>
2024-11-18 21:16:08 -0500

    Add bitcoin tests

---
+++ src/test/java/ca/yorku/cmg/cnsim/bitcoin/BehaviorTest.java
@@ -0,0 +1,97 @@
+//// Compilation error
+//package ca.yorku.cmg.cnsim.bitcoin;
+//
+//import ca.yorku.cmg.cnsim.bitcoin.BitcoinNode;
+//import ca.yorku.cmg.cnsim.bitcoin.BitcoinNodeFactory;
+//import ca.yorku.cmg.cnsim.bitcoin.Block;
+//import ca.yorku.cmg.cnsim.bitcoin.MaliciousNodeBehavior;
+//import ca.yorku.cmg.cnsim.engine.*;
+//import ca.yorku.cmg.cnsim.engine.network.AbstractNetwork;
+//import ca.yorku.cmg.cnsim.engine.network.RandomEndToEndNetwork;
+//import ca.yorku.cmg.cnsim.engine.node.AbstractNodeFactory;
+//import ca.yorku.cmg.cnsim.engine.node.INode;
+//import ca.yorku.cmg.cnsim.engine.node.NodeSet;
+//import ca.yorku.cmg.cnsim.engine.transaction.Transaction;
+//import ca.yorku.cmg.cnsim.engine.transaction.TransactionWorkload;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.Assert.assertFalse;
+//import static org.junit.jupiter.api.Assertions.assertFalse;
+//
+//public class BehaviorTest {
+//    private AbstractSampler sampler;
+//    private AbstractNetwork n;
+//    private Simulation s;
+//    private NodeSet ns;
+//    private AbstractNodeFactory nf;
+//    private Transaction target;
+//
+//    @BeforeEach
+//    void setUp() throws Exception {
+//        Config.init("./CNSim/tests/BitcoinNode/Case 1 - config.txt");
+//        sampler = new FileBasedSampler("./CNSim/resources/transactions.csv", "./CNSim/resources/nodes.csv");
+//    }
+//
+//    @AfterEach
+//    void tearDown() throws Exception {
+//    }
+//
+//
+//    @Test
+//    void testDoubleSpendingAttack() throws Exception{
+//
+//        Simulation s = new Simulation();
+//        s.setSampler(sampler);
+//        AbstractNodeFactory nf = new BitcoinNodeFactory("Honest", s);
+//        NodeSet ns = new NodeSet(nf);
+//        ns.addNodes(3);
+//
+//        //ns.addNodes(Config.getPropertyInt("net.numOfNodes"));
+//        AbstractNetwork n = new RandomEndToEndNetwork(ns, sampler);
+//        s.setNetwork(n);
+//
+//        TransactionWorkload ts = new TransactionWorkload(sampler);
+//        ts.appendTransactions(100);
+//        s.schedule(ts);
+//
+//        // Assign a target transaction for malicious behavior
+//        System.out.println(ts.printIDs(","));
+//        for (INode node : ns.getNodes()) {
+//            if (node instanceof BitcoinNode) {
+//                BitcoinNode bNode = (BitcoinNode) node;
+//                if (bNode.getBehaviorStrategy() instanceof MaliciousNodeBehavior) {
+//                    ((MaliciousNodeBehavior) bNode.getBehaviorStrategy()).setTargetTransaction(ts.getTransaction(15));
+//                    target = ts.getTransaction(15);
+//                }
+//            }
+//        }
+//
+//        s.run();
+//        long realTime = (System.currentTimeMillis() - Profiling.simBeginningTime); // in Milli-Sec
+//        System.out.printf("\n");
+//        System.out.println("Real time(ms): " + realTime);
+//        System.out.println("Simulation time(ms): " + Simulation.currTime);
+//
+//        s.getNodeSet().closeNodes();
+//
+//        //print the blockchain structure after simulation
+//        ns.getNodes().forEach(node -> {
+//            BitcoinNode bNode = (BitcoinNode) node;
+//            System.out.println(bNode.blockchain.printLongestChain());
+//        });
+//
+//        //check if the target transaction is in the final structure
+//        ns.getNodes().forEach(node -> {
+//            BitcoinNode bNode = (BitcoinNode) node;
+//            if(bNode.blockchain.contains(target)){
+//                System.out.println("Transaction 15 is in the final structure");
+//                Block longestBlock = bNode.blockchain.getLongestTip();
+//                //assertFalse(bNode.blockchain.contains(longestBlock));
+//            }
+//        });
+//
+//    }
+//}
+//
